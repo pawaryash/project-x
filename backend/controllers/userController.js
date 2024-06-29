587cd6a21b381052e5106020ad3a60276c1f3a48
pawaryash <er.yashpawar@gmail.com>
2024-06-29 13:02:31 +0530

    chore: Added logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,3 +43,17 @@ exports.loginUser = catchAsyncError(async (req, res, next)=>{
     sendToken(user,201,res);
 });
 
+//Logout User
+exports.logout = catchAsyncError(async(req, res, next)=>{
+
+    res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out",
+    });
+});
+
