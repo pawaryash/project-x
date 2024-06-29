7401bf7fdafaef2a5647faefc052e7550f129723
pawaryash <er.yashpawar@gmail.com>
2024-06-29 12:38:29 +0530

    feat: Add authentication middleware for verifying user token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,20 @@
+const ErrorHandler = require("../utils/errorHandler")
 const catchAsyncError = require("./catchAsyncError");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncError(async(req, res, next)=>{
-    const token = req.cookies;
+    const {token} = req.cookies;
 
-    console.log(token);
+    // console.log(token);
+    if(!token){
+        return next(new ErrorHandler("Please login to access this resource", 401));
+    }
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decodedData.id);
+
+    next();
+
 });
